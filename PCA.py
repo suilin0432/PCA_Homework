e4ae00ffdb0b958345437cf690cacc3dbf2a855c
隋霖 <401569798@qq.com>
2019-06-01 19:10:52 +0800

    save the data with different k

--- PCA.py
+++ PCA.py
@@ -3,27 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class PCA():
-    def __init__(self, k=4):
+    def __init__(self, data, k=4):
         self.k = k
+        self.data = data
         pass
 
     def run(self):
         # 获取数据
-        data = Get_Data_Class()
-        m, n = data.shape
+        # shape: 1024 * 8
+        m, n = self.data.shape
         # 进行数据的居中
-        MEAN = self.mean(data)
-        data = data - MEAN
+        MEAN = self.mean(self.data)
+        # shape: 1024 * 8
+        data = self.data - MEAN
         # 计算居中矩阵的协方差矩阵
-        covX = np.cov(data)
-        featValue, featVec = np.linalg.eig(covX.T)
+        # shape: 8*8
+        covX = np.cov(data.T)
+        featValue, featVec = np.linalg.eig(covX)
         index = np.argsort(-featValue)
         finalData = []
         if self.k > n:
             print("K 应该小于 n")
             return
+        # shape: k*8
         selectVec = np.matrix(featVec.T[index[:self.k]])
-        finalData = data.T * selectVec.T
+        # 1024*8 * 8*k -> shape: 1024 * k
+        finalData = data * selectVec.T
+        # 1024*k * k*8 -> shape 1024 * 8
         reconData = (finalData * selectVec) + MEAN
         return finalData, reconData
 
@@ -32,10 +38,15 @@ class PCA():
         return np.mean(data)
 
 
-pca = PCA()
-finalData, reconData = pca.run()
-# print(reconData)
-# data = Get_Data_Class()
-# print(np.sum(np.square(reconData-data.T)))
-print(reconData.shape)
+data = Get_Data_Class()
+pca = PCA(data=data)
+print(data)
 
+for i in range(1, 9):
+    # file = open("PCA_k_{}.txt".format(i), "w")
+    # shape: 1024 * k, 1024 * 8
+    pca.k = i
+    finalData, reconData = pca.run()
+    np.savetxt("PCA_k_{}.txt".format(i), finalData)
+    # 计算差异
+    print(np.sum(np.square(reconData-data)))
